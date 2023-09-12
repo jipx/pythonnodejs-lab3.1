7ee931a4afac76625c4125b7ebe27253a65538fb
EC2 Default User <ec2-user@ip-10-16-10-169.ec2.internal>
2023-09-12 06:44:30 +0000

    change path to policy file

--- python_3/permissions.py
+++ python_3/permissions.py
@@ -7,7 +7,7 @@ import json
 S3API = boto3.client("s3", region_name="us-east-1")
 bucket_name = "jipx-2023-09-12"
 
-policy_file = open("/home/ec2-user/environment/website_security_policy.json", "r")
+policy_file = open("../website_security_policy.json", "r")
 
 
 S3API.put_bucket_policy(
