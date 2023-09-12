7b1360fd8ea9f32b09dbc2fb768a1b91ba68a615
EC2 Default User <ec2-user@ip-10-16-10-160.ec2.internal>
2023-09-12 04:15:48 +0000

    add nodejs code

---
+++ nodejs/permissions.js
@@ -0,0 +1,22 @@
+
+
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const s3 = new AWS.S3({ region: 'us-east-1' });
+const bucketName = 'jipx-2023-09-12';
+
+const policyFile = fs.readFileSync('/home/ec2-user/environment/website_security_policy.json', 'utf8');
+
+const params = {
+  Bucket: bucketName,
+  Policy: policyFile
+};
+
+s3.putBucketPolicy(params, (err, data) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Policy uploaded successfully.');
+  }
+});
